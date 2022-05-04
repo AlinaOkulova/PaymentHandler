9a6e66e94dbc40ba259f8ff5fd60219e138a0e85
AlinaOkulova <a.v.okulova@gmail.com>
2022-05-05 02:20:11 +0300

    update

--- src/main/java/com/example/handler2/configuration/SpringAsyncConfig.java
+++ src/main/java/com/example/handler2/configuration/SpringAsyncConfig.java
@@ -1,11 +1,13 @@
 package com.example.handler2.configuration;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -21,5 +23,14 @@ public class SpringAsyncConfig implements AsyncConfigurer {
         executor.setQueueCapacity(50);
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (ex, method, params) -> {
+            System.out.println("Exception: " + ex.getMessage());
+            System.out.println("Method name: " + method.getName());
+            ex.printStackTrace();
+        };
     }
 }
